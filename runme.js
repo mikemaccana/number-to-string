0896ca682df1b1bb53929037a44ee07e21f9cfb3
Mike MacCana <mike.maccana@gmail.com>
2020-02-12 12:38:54 +0000

    Handle larger numbers

--- runme.js
+++ runme.js
@@ -4,6 +4,11 @@ const numberToWords = function(number) {
   if (number.constructor.name !== "Number") {
     throw new Error(`Provide a number`);
   }
+
+  if (number > 999) {
+    throw new Error(`Not implemented`);
+  }
+
   // There's a weird pattern in English here let me think about it
   // Can't Google right now just need to type this shit out
   const onesToWords = [
@@ -46,26 +51,37 @@ const numberToWords = function(number) {
   // We'll append this over time
   var answer = "";
 
-  if (number < 20) {
-    return onesToWords[number];
+  var hundreds = Math.floor(number / 100);
+  var tensAndOnes = number % 100;
+
+  // Check hundreds
+  //
+  // We need to handle the and for hundreds
+  var isMoreAfterHundreds = Boolean(tensAndOnes);
+  if (hundreds) {
+    answer += `${onesToWords[hundreds]} hundred `;
+    if (isMoreAfterHundreds) {
+      answer += `and `;
+    }
   }
 
-  const tens = Math.floor(number / 10);
-  const ones = number % 10;
-
-  if (number < 100) {
-    return `${tensToWords[tens]} ${onesToWords[ones]}`;
+  // Then do tensAndOnes
+  if (tensAndOnes < 20) {
+    answer += onesToWords[tensAndOnes];
+  } else {
+    const tens = Math.floor(tensAndOnes / 10);
+    const ones = tensAndOnes % 10;
+    answer += `${tensToWords[tens]} ${onesToWords[ones]}`;
   }
 
-  throw new Error(`Not implemented`);
+  return answer;
 };
 
 // Trying some small numbers first
 // I really should use mocha, expect etc as it'd return expecyted vs actual
 
-// log(numberToWords(1));
-// log(numberToWords(10));
-// log(numberToWords(11));
-// Adding this because it tests more logic
+log(numberToWords(1));
+log(numberToWords(10));
+log(numberToWords(11));
 log(numberToWords(99));
-// log(numberToWords(111));
+log(numberToWords(111));
